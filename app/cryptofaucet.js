4b0f823551ce59a75ccbd673e4d22c2d678b5daa
Kevin Colgan <kevinhcolgan@gmail.com>
2018-09-19 17:35:36 +0200

    implementation of send Crypto using env var for wallet passphrase

--- app/cryptofaucet.js
+++ app/cryptofaucet.js
@@ -2,18 +2,19 @@ var faucet_constants = require("../app/faucet_constants");
 var cryptofaucet = require("../app/cryptofaucet");
 var defaultCryptoSymbol = faucet_constants.TBTC_SYMBOL;
 
-var tbtcWallets = bitgo.coin(faucet_constants.TBTC_SYMBOL).wallets();
 
-var tbtcFaucetWallet = wallets.get({ "id": faucet_constants.FAUCET_TBTC_WALLET_ID }, function callback(err, wallet) {
-    if (err) {
-        throw err;
-    }
-});;
 exports.getBalance = function(cryptoSymbol) {
     var useSymbol = cryptoSymbol || defaultCryptoSymbol;
     if(useSymbol === faucet_constants.TBTC_SYMBOL)
     {
-        return tbtcFaucetWallet.balance();
+        var tbtcWallets = bitgo.coin(faucet_constants.TBTC_SYMBOL).wallets();
+        tbtcWallets.get({ "id": faucet_constants.FAUCET_TBTC_WALLET_ID }, function callback(err, wallet) {
+            if (err) {
+                throw err;
+            }
+            return wallet.balance();
+        });
+
     }
     //TODO add implementation for other supported crypto symbols like tETH etc.
 }
@@ -25,18 +26,29 @@ exports.sendCrypto = function(cryptoSymbol,rxAddress) {
     var tx = null;
     if(useSymbol === faucet_constants.TBTC_SYMBOL)
     {
+        var tbtcWallets = bitgo.coin(faucet_constants.TBTC_SYMBOL).wallets();
         //the default granularity of amount for tBTC is satoshi
         let params = {
-            amount: faucet_constants.FAUCET_SEND_AMOUNT_TBTC,
-            address: '2NFfxvXpAWjKng7enFougtvtxxCJ2hQEMo4',
-            walletPassphrase: 'secretpassphrase1a5df8380e0e30'
+            amount: faucet_constants.FAUCET_SEND_AMOUNT_TBTC * 1e8,
+            address: rxAddress,
+            walletPassphrase: process.env.TBTC_FAUCET_WALLET_PASSPHRASE
         };
-        tbtcFaucetWallet.send(params)
-            .then(function(transaction) {
-                // print transaction details
-                console.dir(transaction);
-                tx = transaction;
-            });
+        tbtcWallets.get({ "id": faucet_constants.FAUCET_TBTC_WALLET_ID }, function callback(err, wallet) {
+            if (err) {
+                throw err;
+            }
+
+
+            console.log("Balance is: " + (wallet.balance() / 1e8).toFixed(4));
+            console.log("Attempting to send : " + (params.amount / 1e8).toFixed(4));
+            //console.log(JSON.stringify(wallet));
+
+            wallet.send(params)
+                .then(function(transaction) {
+                    // print transaction details
+                    console.dir(transaction);
+                });
+        });
     }
 
 
