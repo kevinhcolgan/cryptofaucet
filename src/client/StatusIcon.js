b445401e320f40b7bfa6688a643117f91fe9cf64
Kevin Colgan <kevinhcolgan@gmail.com>
2018-09-24 10:14:39 +0200

    added components for spinner and status icon

--- src/client/StatusIcon.js
+++ src/client/StatusIcon.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faSpinner } from '@fortawesome/free-solid-svg-icons'
+import { faCheckCircle } from '@fortawesome/free-solid-svg-icons'
+import { faExclamationCircle } from '@fortawesome/free-solid-svg-icons'
 
-library.add(faSpinner)
-
-const LoadingSpinner = () => (
-    <FontAwesomeIcon className="fa-spin" icon="spinner" />
+library.add(faCheckCircle)
+library.add(faExclamationCircle)
+let useIcon = '';
+const StatusIcon = (statusCode) => (
+    if(statusCode === 'tx_success')
+    {
+        useIcon = `${faCheckCircle}`
+    }
+    else if(statusCode === 'tx_failed') {
+        useIcon = `${faExclamationCircle}`
+    }
+    <FontAwesomeIcon className="fa-spin" icon={useIcon} />
 );
 
-export default LoadingSpinner;
+export default StatusIcon;
