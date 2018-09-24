49f5dbfc0e7bafc7fd723523671a8417917ad52f
Kevin Colgan <kevinhcolgan@gmail.com>
2018-09-24 10:11:49 +0200

    added loading spinner and message icons depending on statusCode from server. Experimented with using async for fetch calls but ran into browser problems, so stick to simple fetch

--- src/client/App.js
+++ src/client/App.js
@@ -1,5 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
+import LoadingSpinner from './LoadingSpinner';
+import { library } from '@fortawesome/fontawesome-svg-core'
+import {faCheckCircle, faExclamationCircle, faSpinner} from "@fortawesome/free-solid-svg-icons";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+
+library.add(faCheckCircle)
+library.add(faExclamationCircle)
 
 class App extends Component {
     constructor(props) {
@@ -9,42 +16,126 @@ class App extends Component {
             txid: null,
             isLoading: null,
             statusMessage: null,
-            appStatus: null
+            statusCode: null,
+            address:null
         };
         this.handleSubmit = this.handleSubmit.bind(this);
+        this.getStatusIcon = this.getStatusIcon.bind(this);
     }
+    //https://www.valentinog.com/blog/how-async-await-in-react/
+    /*
+    async componentDidMount() {
+
+        try {
+            const response = await fetch('/api/getBalance');
+            const data = await response.json();
+
+            this.setState(() => ({
+                balance: data.balance
+            }));
+        } catch (error) {
+            console.log(error);
+        }
+
+    }
+    */
 
     componentDidMount() {
+
         fetch('/api/getBalance')
             .then(res => res.json())
-            .then(data => this.setState({ balance: data.balance, address: null, txid: null }));
+            .then(data => this.setState(() => ({
+                balance: data.balance
+            })));
+
     }
 
+
     handleSubmit(event) {
-        this.setState(() => ({
-            isLoading: true
-        }));
+
         event.preventDefault();
+
+        const data = new FormData(event.target);
+        const address = data.get('address');
+        const cryptoSymbol = data.get('cryptoSymbol');
+        this.setState(() => ({
+            isLoading: true,
+            statusMessage: `Attempting to send 0.0001 ${cryptoSymbol.toUpperCase()} to ${address}`
+        }));
+
+
+        fetch(`/api/sendCrypto?cryptoSymbol=${cryptoSymbol}&clientAddress=${address}`)
+            .then(res => res.json())
+            .then(data => this.setState(() => ({
+                balance: data.balance,
+                txid: data.txid,
+                isLoading: false,
+                statusMessage: data.statusMessage,
+                statusCode: data.statusCode,
+                address:data.address
+                }
+            ))).catch((error) => {
+                this.setState(() => ({
+                    isLoading: false
+                }));
+            });
+
     }
+
+    getStatusIcon(statusCode,isLoading) {
+        let useIcon = '';
+        if(isLoading)
+        {
+            return <FontAwesomeIcon icon=''/>
+        }
+        else
+        {
+            if (statusCode === 'tx_success') {
+                useIcon = `${faCheckCircle.iconName}`
+            }
+            else if (statusCode === 'tx_failed') {
+                useIcon = `${faExclamationCircle.iconName}`
+            }
+            return <FontAwesomeIcon icon={useIcon}/>
+        }
+
+
+    }
+
     render() {
         const data = this.state;
         return (
             <div className="App">
                 <h1>CryptoFaucet tBTC available:
-                    <span className='balance'>{data.balance ? ` ${data.balance} ` : ' - '}</span>
+                    <span className='balance'>{data.balance ? ` ${data.balance} ` : data.isLoading ? <LoadingSpinner /> : ' - '}</span>
                 </h1>
-                <form className='faucet' id="faucet-request" onSubmit={this.handleSubmit}>
-                    <input type="hidden" name="cryptoSymbol" id="cryptoSymbol" />
-                    <label htmlFor="address">
-                        Address:
-                        <input type="text" id="address" name="address" onChange={this.handleAddressChange}/>
-                    </label>
-                    <input type='hidden' className='txid' value={data.txid || ''}/>
-                    <button type='submit' disabled={data.isLoading}>Submit</button>
-                </form>
+                <div className={'faucetFormContainer'}>
+                    <form className='faucet' id="faucet-request" onSubmit={this.handleSubmit}>
+                        <div className={'formElement'}>
+                            <label htmlFor="cryptoSymbol">
+                                Crypto Currency:
+                                <select name="cryptoSymbol">
+                                    <option value='tbtc'>TBTC</option>
+                                </select>
+                            </label>
+                        </div>
+                        <div className={'formElement'}>
+                            <label htmlFor="address">
+                                Address:
+                                <input type="text" id="address" name="address" onChange={this.handleAddressChange} />
+                            </label>
+                            <input type='hidden' className='txid' value={data.txid || ''}/>
+                            <button type='submit' disabled={data.isLoading}>Submit</button>
+                        </div>
 
-                <p className='status-message'>{data.statusMessage ? `${data.statusMessage}` : ''}</p>
 
+                    </form>
+
+                    <p className={data.statusCode ? `formElement status-message ${data.statusCode}` : 'formElement status-message'}>
+                        {data.isLoading ? <LoadingSpinner /> : ''}
+                        {data.statusCode ? this.getStatusIcon(data.statusCode,data.isLoading) : ''}
+                        {data.statusMessage ? `${data.statusMessage}` : ''}</p>
+                </div>
             </div>
         );
     }
